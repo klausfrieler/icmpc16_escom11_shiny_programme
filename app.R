41307f201ac5e47e17febae4bb4c351d8e14dde8
klaus <kgf@omniversum.de>
2021-07-27 13:57:57 +0200

    Version 0.1

--- app.R
+++ app.R
@@ -4,74 +4,70 @@
 #
 # Find out more about building applications with Shiny here:
 #
-#    http://shiny.rstudio.com/
+#    http://shiny.rstudio.com/ 
 #
 library(tidyverse)
+library(DT)
 library(shiny)
-messagef <- function(...) messagef(sprintf(...))
-
-slot_codes <- c("P" = "Poster", 
-                "S" = "Symposium", 
-                "W" =  "Workshop", 
-                "K" = "Keynote",
-                "C" = "Concert", 
-                "1" = "Talk", 
-                "2" = "Talk", 
-                "3" = "Talk", 
-                "4" = "Talk")
-setup_workspace <- function(fname = "ICMPC-ESCOM-2021-Programme.csv"){
-    
-    master <- readr::read_csv(fname)
-    names(master) <- c("day", "time_utc", "strand", "room", "order", "theme", "authors", "title", "abstract_id", 
-                       "country")
-    master <- master %>% 
-        mutate(authors = str_replace(authors, "\\([0-9,]+\\)", "")) %>% 
-#        mutate(first_authors = str_split(authors, ";") %>% map_chr(., ~{.x[[1]][1]})) %>% 
-        mutate(full_name = str_split(authors, ";")) %>%
-        unnest(full_name) %>% 
-        mutate(last_name = str_split(full_name, ",") %>% map_chr(., ~{.x[[1]][1]})) %>% 
-        mutate(full_name = trimws(full_name)) %>% 
-        mutate(last_name = trimws(last_name)) %>% 
-        mutate(time_utc =  str_replace_all(time_utc, "\\([a-zA-Z]+\\)", "") %>% trimws() %>% as.numeric()) %>% 
-        mutate(time_cest = time_utc  + 2, 
-               time_gmt = time_utc + 1, 
-               time_ist = time_utc  + 5.5, 
-               time_cdt = time_utc - 5,
-               time_aest = time_utc + 10) %>% 
-        mutate(slot_type = substr(room, 1, 1), poster_slot = substr(room, 2, 2)) %>% 
-        mutate(slot_type_long = slot_codes[slot_type])
-
-    assign("master", master, globalenv())
-}
+source("utils.R")
 setup_workspace()
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("ICMPC11/ESCOM16 Progamme"),
+    titlePanel("ICMPC16/ESCOM11 Progamme"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
             selectInput(inputId = "filter_name", 
-                        label = "Last Name",
-                        choices = unique(master$full_name), selected = "Alluri, Vinoo",
-                        multiple = T, selectize = T),
+                        label = "Author",
+                        choices = c("All", sort(unique(master$full_name))), selected = "All",
+                        multiple = F, selectize = T),
+            selectInput(inputId = "theme_cleaned", 
+                        label = "Theme",
+                        choices = c("All", sort(unique(master$theme_cleaned))), selected = "All",
+                        multiple = F, selectize = T),
             selectInput(inputId = "slot_type", 
                         label = "Slot Type",
-                        choices = c("All", unique(master$slot_type_long)), selected = "All",
+                        choices = c("All", sort(unique(master$slot_type_long))), selected = "All",
+                        multiple = F, selectize = T),
+            selectInput(inputId = "day", 
+                        label = "Day",
+                        choices = c("All", unique(master$day)), selected = "All",
+                        multiple = F, selectize = T),
+            selectInput(inputId = "hub", 
+                        label = "Time Hub",
+                        choices = c("All", unique(master$hub)), selected = "All",
                         multiple = F, selectize = T),
             selectInput(inputId = "time_zone", 
                         label = "Time ",
-                        choices = c(names(master %>% select(starts_with("time")))), selected = "time_utc",
+                        choices = tz, selected = "time_utc",
                         multiple = F, selectize = T),
+            p(
+                "ICMPC/ESCOM 2021 programme browser v0.1", 
+                shiny::tags$br(), 
+                shiny::tags$br(), 
+                "Author: Klaus Frieler, Max Planck Institute for Empicial Aesthetics, Frankfurt/M, Germany",
+                shiny::tags$br(), 
+                shiny::tags$br(),
+                "Data provided by Mike Weiss", 
+                shiny::tags$br(),
+                shiny::tags$br(), 
+                "Powered by the Deutsche Gesellschaft für Musikspsychologie",
+                shiny::tags$br(),
+                shiny::tags$br(), 
+                "Hint: All drop boxes have incremental search function.",
+                style = "font-size: 10pt; display: block"
+            ),
             width = 2
             
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           tableOutput("programme")
+            DT::DTOutput("programme")
         )
     )
 )
@@ -79,15 +75,34 @@ setup_workspace()
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$programme <- renderTable({
+    output$programme <- renderDataTable({
         # generate bins based on input$bins from ui.R
         #browser()
-        data <- master %>% filter(full_name %in% input$filter_name) 
+        data <- master
+        if(input$day != "All"){
+            data <- data %>% 
+                filter(day %in% input$day) 
+        }
+        if(input$filter_name != "All"){
+            data <- data %>% 
+                filter(full_name %in% input$filter_name) 
+        }
+        if(input$hub != "All"){
+            data <- data %>% 
+                filter(hub %in% input$hub) 
+        }
+        if(input$theme_cleaned != "All"){
+            data <- data %>% 
+                filter(theme_cleaned %in% input$theme_cleaned) 
+        }
         if(input$slot_type != "All"){
-            data <- data %>% filter(slot_type_long %in% input$slot_type_long) 
+            data <- data %>% filter(slot_type_long %in% input$slot_type) 
             
         }
-        data %>% select(input$time_zone, !starts_with("time"), -slot_type, -last_name, -full_name, -country, )
+        data %>% 
+            select(input$time_zone, !starts_with("time")) %>% 
+            distinct(authors, title, .keep_all = T) %>% 
+            select(-full_name, -first_author, -last_author, -hub, -theme, theme = theme_cleaned, -slot_type, type = slot_type_long, -last_name, -poster_slot, -country)
     })
 }
 
---
+++ utils.R
@@ -0,0 +1,66 @@
+messagef <- function(...) messagef(sprintf(...))
+format_time <- function(time){
+  suffix <- rep("00", length(time))
+  suffix[floor(time) != time] <- "30"
+  sprintf("%02d:%s", as.integer(floor(time)), suffix)
+}
+slot_codes <- c("P" = "Poster", 
+                "S" = "Symposium", 
+                "W" =  "Workshop", 
+                "K" = "Keynote",
+                "C" = "Concert", 
+                "1" = "Talk", 
+                "2" = "Talk", 
+                "3" = "Talk", 
+                "4" = "Talk")
+time_zones <- c("time_utc" = "Universal Time", "time_cest" = "Central European Summer Time",
+                "time_ist" = "Indian Standard Time", "time_gmt" = "Greenwich Mean Time",
+                "time_cdt" = "Central Daylight Time", 
+                "time_aest" = "Australian Eastern Standard Time")
+tz <- names(time_zones)
+names(tz) <- time_zones
+get_last_author <- function(authors){
+  if(length(authors) > 1){
+    return(map_chr(authors, function(x) get_last_author(x)))
+  }
+  elems <- str_split(authors, ";")[[1]] %>% trimws()
+  if(length(elems) < 3){
+    return(NA)
+  }
+  elems[length(elems)]
+  
+}
+setup_workspace <- function(fname = "ICMPC-ESCOM-2021-Programme.csv"){
+  
+  master <- readr::read_csv(fname)
+  names(master) <- c("day", "time_utc", "strand", "room", "order", "theme", "authors", "title", "abstract_id", 
+                     "country")
+  master <- master %>% 
+    mutate(authors = str_replace(authors, "\\([0-9,]+\\)", "")) %>% 
+    mutate(authors = str_replace(authors,"\\\r\\\n", "")) %>% 
+    mutate(first_author = str_split(authors, ";") %>% map_chr(., ~{.x[[1]][1] %>% trimws()})) %>% 
+    mutate(last_author = get_last_author(authors)) %>% 
+    mutate(full_name = str_split(authors, ";")) %>%
+    unnest(full_name) %>% 
+    mutate(last_name = str_split(full_name, ",") %>% map_chr(., ~{.x[[1]][1]})) %>% 
+    mutate(full_name = trimws(full_name)) %>% 
+    mutate(last_name = trimws(last_name)) %>% 
+    mutate(time_utc =  str_replace_all(time_utc, "\\([a-zA-Z]+\\)", "") %>% trimws() %>% as.numeric()) 
+    master <- master %>%
+    mutate(hub = case_when(time_utc >= 8 & time_utc <= 18 ~ "08:00-18:00", 
+                                  time_utc >= 23 | time_utc <= 2 ~ "23:00-02:00", 
+                                  TRUE ~ "05:00-07:00")) %>% 
+    mutate(time_cest = (time_utc  + 2) %% 24, 
+           time_gmt = (time_utc + 1)  %% 24, 
+           time_ist = (time_utc  + 5.5)  %% 24, 
+           time_cdt = (time_utc - 5)  %% 24,
+           time_aest = (time_utc + 10) %% 24) %>% 
+    mutate_at(vars(starts_with("time")), format_time) %>% 
+    mutate(slot_type = substr(room, 1, 1), poster_slot = substr(room, 2, 2)) %>% 
+    mutate(slot_type_long = slot_codes[slot_type]) %>% 
+    mutate(theme_cleaned = str_replace(theme, "\\([A-Z]{1}\\)", "") %>% 
+             str_replace("\\\r\\\n", " ") %>% 
+             str_replace("\\[[a-zA-Z ]+\\]", "") %>% trimws())
+  
+  assign("master", master, globalenv())
+}
